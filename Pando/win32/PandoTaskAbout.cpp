4b970f2a3b27adf5c3b48ae7f5e8ad817d8af40d
ForceStudio <ipvb@qq.com>
2014-09-09 23:36:20 +0800

    Signed-off-by: ForceStudio <ipvb@qq.com>

---
+++ Pando/win32/PandoTaskAbout.cpp
@@ -0,0 +1,20 @@
+/*********************************************************************************************************
+* PandoTaskAbout.cpp
+* Copyright (C) 2014 The ForceStudio All Rights Reserved.
+* Note: Pando Editor Taskdialog About
+* E-mail:<forcemz@outlook.com>
+* @2014.09
+**********************************************************************************************************/
+#include <Windows.h>
+
+
+
+LRESULT WINAPI    PandoTaskAboutShow()
+{
+ return 0;
+}
+
+
+
+
+
